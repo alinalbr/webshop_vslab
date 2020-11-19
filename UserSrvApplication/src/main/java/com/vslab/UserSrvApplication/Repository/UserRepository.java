d6736f4c04a1c330e6bd209e1cc0093d5d83a26d
Nina Schulz <nschulz@inovex.de>
2020-11-19 14:39:56 +0100

    deleted user src

--- UserSrvApplication/src/main/java/com/vslab/UserSrvApplication/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vslab.UserSrvApplication.Repository;
-import com.vslab.UserSrvApplication.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findOne(Long userId);
-
-}
