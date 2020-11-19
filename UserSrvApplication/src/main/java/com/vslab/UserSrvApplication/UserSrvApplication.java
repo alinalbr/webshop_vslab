ee76771a36c98e3676be6ca759b940b06d67112a
alinalbr <alina.leber@web.de>
2020-11-19 11:40:07 +0100

    heheh

---
+++ UserSrvApplication/src/main/java/com/vslab/UserSrvApplication/UserSrvApplication.java
@@ -0,0 +1,13 @@
+package com.vslab.UserSrvApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSrvApplication.class, args);
+	}
+
+}
---
+++ UserSrvApplication/src/test/java/com/vslab/UserSrvApplication/UserSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vslab.UserSrvApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
