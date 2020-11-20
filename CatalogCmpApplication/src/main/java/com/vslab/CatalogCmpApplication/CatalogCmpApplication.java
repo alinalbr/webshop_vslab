300e802c26217a8157a34b90dbcdab11622ef915
much1030 <64838212+much1030@users.noreply.github.com>
2020-11-20 16:29:42 +0100

    Update CatalogCmpApplication.java

--- CatalogCmpApplication/src/main/java/com/vslab/CatalogCmpApplication/CatalogCmpApplication.java
+++ CatalogCmpApplication/src/main/java/com/vslab/CatalogCmpApplication/CatalogCmpApplication.java
@@ -2,8 +2,18 @@ package com.vslab.CatalogCmpApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@RibbonClient("catalog-cmp-service")
+
 public class CatalogCmpApplication {
 
 	public static void main(String[] args) {
