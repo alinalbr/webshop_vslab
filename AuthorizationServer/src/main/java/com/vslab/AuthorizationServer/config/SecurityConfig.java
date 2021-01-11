4016eeab60666dbad91879bd5e9c8c2cf7e63546
Nina Schulz <nschulz@inovex.de>
2021-01-11 15:45:31 +0100

    wip: login via resttemplate

--- AuthorizationServer/src/main/java/com/vslab/AuthorizationServer/config/SecurityConfig.java
+++ AuthorizationServer/src/main/java/com/vslab/AuthorizationServer/config/SecurityConfig.java
@@ -1,20 +1,22 @@
 package com.vslab.AuthorizationServer.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author Joe Grandja
  */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     // @formatter:off
     @Override
@@ -28,7 +30,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
     // @formatter:on
 
-    @Bean
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    /*@Bean
     public UserDetailsService users() throws Exception {
         @SuppressWarnings("deprecation")
         User.UserBuilder users = User.withDefaultPasswordEncoder();
@@ -38,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
 
         return manager;
-    }
+    }*/
 
     @Bean
     @Override
